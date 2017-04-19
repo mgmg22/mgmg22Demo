b459b1055f82b90462fe073f4735730b72c4503f
uniware <d33hc@qq.com>
2017-04-19 10:46:05 +0800

    add BottomNavigationView

--- app/src/main/java/com/google/mgmg22demo/activity/MainActivity.java
+++ app/src/main/java/com/google/mgmg22demo/activity/MainActivity.java
@@ -2,16 +2,23 @@ package com.google.mgmg22demo.activity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
 
 import com.google.mgmg22demo.R;
 import com.google.mgmg22demo.base.BaseActivity;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
-//    @BindView(R.id.btn_01)
+    //    @BindView(R.id.btn_01)
 //    Button btn_01;
+    @BindView(R.id.bottomNavigationView)
+    BottomNavigationView mBottomNavigationView;
+    private int mCurrPosition;
 
     @OnClick(R.id.btn_01)
     void start() {
@@ -39,5 +46,28 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initView();
     }
+
+    private void initView() {
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                mCurrPosition = item.getItemId();
+                switch (item.getItemId()) {
+                    case R.id.item_home_page:
+//                        showHideFragment(0);
+                        break;
+                    case R.id.item_category:
+//                        showHideFragment(1);
+                        break;
+                    case R.id.item_mine:
+//                        showHideFragment(2);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
 }
