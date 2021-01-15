718740cbf86487601a66efbff7abba57ca7d4bee
沈晓顺 <shenxs@weipaitang.com>
2021-01-15 14:53:35 +0800

    动态控制是否拦截侧滑返回

--- libs/lib_slide_back/src/main/java/com/google/mgmg22/lib_slide_back/OnInterceptSwipeBack.java
+++ libs/lib_slide_back/src/main/java/com/google/mgmg22/lib_slide_back/OnInterceptSwipeBack.java
@@ -1,8 +1,12 @@
 package com.google.mgmg22.lib_slide_back;
 
 /**
- * webview页面动态控制是否拦截侧滑返回
+ * 页面动态控制是否拦截侧滑返回
+ * 使用方式：
+ * 1在要拦截侧滑返回的Activity实现该接口
+ * 2在Activity的onCreate添加    SmartSwipe.wrap(this).setOnInterceptSwipeBack(this);
  */
 public interface OnInterceptSwipeBack {
+    //返回值为true时不可侧滑
     boolean isDisallowIntercept();
 }
