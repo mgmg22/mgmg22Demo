2eed080ddfb718cc3e9d08d86409efc2251deacb
沈晓顺 <shenxs@weipaitang.com>
2021-02-20 10:28:08 +0800

    斐波那契数列

--- app/src/main/java/com/google/mgmg22demo/activity/MainActivity.kt
+++ app/src/main/java/com/google/mgmg22demo/activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.google.mgmg22demo.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.mgmg22.extensions.startKtxActivity
 import com.google.mgmg22demo.R
@@ -10,10 +11,41 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.e("sxs","onCreate()")
         setContentView(R.layout.activity_main)
 //        在这里调用的话，apk就无限重启。
 //        android.os.Process.killProcess(android.os.Process.myPid())
-        initView()
+//        initView()
+    }
+
+    override fun onResume() {
+        Log.e("sxs","onResume()")
+        super.onResume()
+    }
+
+    override fun onStart() {
+        Log.e("sxs","onStart()")
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        Log.e("sxs","onRestart()")
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        Log.e("sxs","onPause()")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.e("sxs","onStop()")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.e("sxs","onDestroy()")
+        super.onDestroy()
     }
 
     private fun initView() {
---
+++ app/src/test/java/com/google/mgmg22demo/FibTest.java
@@ -0,0 +1,27 @@
+package com.google.mgmg22demo;
+
+import org.junit.Test;
+
+/**
+ * 剑指 Offer 10- I. 斐波那契数列
+ */
+public class FibTest {
+
+
+    @Test
+    public void test() {
+        System.out.println("" + fib(45));
+    }
+
+    public int fib(int N) {
+        if (N == 0) return 0;
+        if (N == 1 || N == 2) return 1;
+        int[] dp = new int[N+1];
+        dp[1] = dp[2] = 1;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2])%1000000007;
+        }
+        return dp[N];
+    }
+
+}
