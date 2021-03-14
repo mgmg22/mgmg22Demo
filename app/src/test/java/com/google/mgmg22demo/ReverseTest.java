577e8e8424df6c36727721628a78d4aa1fbcf1c8
sxs <mgmg22PS>
2021-03-14 14:37:22 +0800

    k个一组反转链表

--- app/src/test/java/com/google/mgmg22demo/ReverseTest.java
+++ app/src/test/java/com/google/mgmg22demo/ReverseTest.java
@@ -23,7 +23,8 @@ public class ReverseTest {
         l4.next = l5;
         printListNode(l1);
 //        printListNode(reverse(l1));
-        printListNode(reverseNum(l1));
+//        printListNode(reverseNum(l1));
+        printListNode(reverseKGroup(l1, 2));
     }
 
     public void printListNode(ListNode head) {
@@ -49,6 +50,47 @@ public class ReverseTest {
             cur = nxt;
         }
         return pre;
+    }
+
+    /**
+     * 反转区间 [a, b) 的元素，注意是左闭右开
+     */
+    ListNode reverse(ListNode a, ListNode b) {
+        ListNode pre, cur;
+        pre = null;
+        cur = a;
+        // while 终止的条件改一下就行了
+        while (cur != b) {
+            ListNode nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        // 返回反转后的头结点
+        return pre;
+    }
+
+    /**
+     * k个一组反转链表
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) return head;
+        ListNode a, b;
+        a = b = head;
+        for (int i = 0; i < k; i++) {
+            // 不足 k 个，不需要反转，base case
+            if (b == null) return head;
+            b = b.next;
+        }
+        // 反转前 k 个元素
+        ListNode newHead = reverse(a, b);
+        // 递归反转后续链表并连接起来
+        a.next = reverseKGroup(b, k);
+        return newHead;
     }
 
     public ListNode reverse(ListNode head) {
