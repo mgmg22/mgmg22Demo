196dcf08b8a8a4792bb8881e4c877a1f5aba26f5
mgmg22 <d33hcx@qq.com>
2024-01-23 20:44:21 +0800

    kt fib

--- app/src/test/java/com/google/mgmg22demo/FibTest2.kt
+++ app/src/test/java/com/google/mgmg22demo/FibTest2.kt
@@ -7,7 +7,7 @@ class FibTestkt {
         if (n == 0) return 0
         if (n == 1 || n == 2) return 1
 
-        val dp = Array(n + 1, { 0 })
+        val dp = Array(n + 1) { 0 }
         dp[1] = 1
         dp[2] = 1
         for (i in 3..n) {
