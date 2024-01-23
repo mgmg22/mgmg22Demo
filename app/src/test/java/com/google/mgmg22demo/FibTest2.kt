e0b5dbca880d73e7f1e1582e2278f1db76988fc8
mgmg22 <d33hcx@qq.com>
2024-01-23 20:42:11 +0800

    kt fib

---
+++ app/src/test/java/com/google/mgmg22demo/FibTest2.kt
@@ -0,0 +1,24 @@
+package com.google.mgmg22demo
+
+import org.junit.Test
+
+class FibTestkt {
+    fun fibKt(n: Int): Int {
+        if (n == 0) return 0
+        if (n == 1 || n == 2) return 1
+
+        val dp = Array(n + 1, { 0 })
+        dp[1] = 1
+        dp[2] = 1
+        for (i in 3..n) {
+            dp[i] = dp[i - 2] + dp[i - 1]
+        }
+        return dp[n]
+    }
+
+    @Test
+    fun testFibKt() {
+        val sum = fibKt(10)
+        println(sum)
+    }
+}
